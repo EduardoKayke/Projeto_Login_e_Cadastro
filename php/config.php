2be5c5c8110adfe74c5e0b69a330db29f5ac987a
EduardoKayke <hakuzosu.developer@gmail.com>
2022-11-04 12:30:49 -0300

    Arquivo PHP de conexão ao banco de dados

---
+++ php/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// Variáveis para o acesso ao banco de dados.
+$dbHost = '127.0.0.1';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'appitens';
+
+// Conexão PHP para MySQL
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+// Teste de conexão
+if($conexao -> connect_errno){
+    echo "Erro ao tentar conexão com o banco de dados";
+} else {
+    echo "Conexão ao banco de dados efetuada com sucesso";
+}
+
+?>
